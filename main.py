2eefed915fabf3b16d3a0895d0965d98b14ec4cb
Samuel Goldstein <99555794+samgoldst@users.noreply.github.com>
2023-01-16 16:11:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,87 @@
+import copy
+import math
+import random
 import numpy as np
-import pygame
 
-from board import Board
+from cell import Cell
+from food import Food
 
 
-b = Board(800, 800, 500, 50, 25)
-b.simulate(1, 50, .1)
-#define constants
+class Board:
+    def __init__(self, width, height, num_foods, num_cells, reach):
+        self.width = width
+        self.height = height
+        self.reach = reach
+        self.foods = []
+        self.cells = []
+        for i in range(num_foods):
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            self.foods.append(Food(x, y))
+        for i in range(num_cells):
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            first_weights = np.random.uniform(-1, 1, (10, 4))
+            second_weights = np.random.uniform(-1, 1, (2, 10))
+            self.cells.append(Cell(x, y, first_weights, second_weights))
 
-pygame.init()
-screen = pygame.display.set_mode((800,800))
-pygame.display.set_caption('Pygame Template')
+    def simulate(self, generations, timesteps, percentage):
+        print()
+        log = []
+        for i in range(generations):
+            total = 0
+            generation_log = []
+            for j in range(timesteps):
+                # print(f"\r{i}: {j}   ", end="")
+                timestep_log = [[],[]]
+                for c in self.cells:
+                    c.action(*self.closest_food(c))
+                    eaten = self.eat(c)
+                    c.eat(eaten)
+                    total += eaten
+                    timestep_log[0].append([(c.x, c.y), c.color])
+                for f in self.foods:
+                    timestep_log[1].append((f.x, f.y))
+                generation_log.append(timestep_log)
+            log.append(generation_log)
+            print(total, max([c.fitness for c in self.cells]))
+            self.purge(percentage)
+        return log
 
-done = False
+    def closest_food(self, c: Cell):
+        cx, cy = c.x, c.y
+        angle = None
+        min = -1
+        for f in self.foods:
+            distance = math.sqrt((cx - f.x) ** 2 + (cy - f.y) ** 2)
+            if distance < min or min == -1:
+                min = distance
+                angle = math.degrees(math.atan2((cy - f.y), (cx - f.x)))
+        return angle, min
 
-clock = pygame.time.Clock()
+    def eat(self, c: Cell):
+        cx, cy = c.x, c.y
+        eaten = 0
+        for f in self.foods:
+            if math.sqrt((cx - f.x) ** 2 + (cy - f.y) ** 2) < self.reach:
+                f.x, f.y = random.randint(0, self.width), random.randint(0, self.height)
+                eaten += 1
+        return eaten
 
-percentage = .5
+    def data(self):
+        self.cells.sort(key=lambda x: x.fitness, reverse=True)
+        print()
+        for c in self.cells:
+            print(f"{c.fitness}")
 
-while True:
-    gen = b.simulate(1, 720, percentage)[0]
-    cs = b.cells.copy()
-    cs.sort(key=lambda x: x.fitness, reverse=True)
-    top = cs[0]
-    print(top.first_weights, "\n", top.second_weights)
-    percentage *= .98
-    for frame in gen:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-        screen.fill((255, 255, 255))
-
-        for c in frame[0]:
-            pygame.draw.circle(screen, c[1], c[0], 7)
-        for f in frame[1]:
-            pygame.draw.circle(screen, (0, 255, 0), f, 7)
-
-        pygame.display.update()
-        clock.tick(60)
-pygame.quit()
+    def purge(self, percentage):
+        self.cells.sort(key=lambda x: x.fitness, reverse=True)
+        self.cells = self.cells[:len(self.cells)//2]
+        self.cells += copy.deepcopy(self.cells)
+        for c in self.cells:
+            c.x = random.randint(100, 700)
+            c.y = random.randint(100, 700)
+            c.change(.1)
+        for f in self.foods:
+            f.x  = random.randint(0, 800)
+            f.y = random.randint(0, 800)
